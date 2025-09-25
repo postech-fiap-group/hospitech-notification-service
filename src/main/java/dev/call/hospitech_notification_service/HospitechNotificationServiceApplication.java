c0601553692671e2b5ab951a0412468f6cb79e15
armanoalves <armanobarrosalvesh@gmail.com>
2025-09-25 17:12:21 -0300

    first commit

---
+++ src/main/java/dev/call/hospitech_notification_service/HospitechNotificationServiceApplication.java
@@ -0,0 +1,13 @@
+package dev.call.hospitech_notification_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitechNotificationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitechNotificationServiceApplication.class, args);
+	}
+
+}
